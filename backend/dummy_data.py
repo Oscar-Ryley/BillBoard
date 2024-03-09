b6618f47be723f56972b3a04e516785a86910318
Erik Lippert <elippert1@sheffield.ac.uk>
2024-03-09 14:46:39 +0000

    aaa

--- backend/dummy_data.py
+++ backend/dummy_data.py
@@ -11,35 +11,36 @@ db = client["hackathon"]
 listings = db["listings"]
 
 if __name__ == "__main__":
-    listings.insert_one({
-        "bookID": 9780262367509,
-        "editionID": 4,
-        "title": "Introduction to Algorithms, Fourth Edition",
-        "description": "A comprehensive update of the leading algorithms text, with new material on matchings in bipartite graphs, online algorithms, machine learning, and other topics.",
-        "price": 150.00,
-        "image": "https://mit-press-us.imgix.net/covers/9780262046305.jpg",
-        "date": datetime.now(),
-        "offers": [
-            {
-                "price": 60.00,
-                "date": datetime.now(),
-                "seller": 1,
-                "condition": "new",
-                "notes": None,
-                "location": "Sheffield"
-            }
-        ],
-        "prices": [
-            {
-                "date": datetime.now(),
-                "new": 50.00,
-                "used": 35.00,
-                "average": 40.23
-            }
-        ],
-        # "editions": [2, 3]  # If this is present, there MUST be corresponding listings in the db with the same version and book number
-    })
+    # listings.insert_one({
+    #     "bookID": 1, # ISBN
+    #     "editionID": 4, # There can be several editions of the same book
+    #     "title": "Introduction to Algorithms, Fourth Edition",
+    #     "description": "A comprehensive update of the leading algorithms text, with new material on matchings in bipartite graphs, online algorithms, machine learning, and other topics.",
+    #     "price": 150.00, # Market price (not our price)
+    #     "image": "https://mit-press-us.imgix.net/covers/9780262046305.jpg",
+    #     "date": datetime.now(), # Date of book release 
+    #     "offers": [
+    #         {
+    #             "price": 60.00,
+    #             "date": datetime.now(), # date of posting
+    #             "seller": 1,
+    #             "condition": "new", # Either new or used
+    #             "notes": None, # Optional string. Meant for things like "First page missing"
+    #             "location": "Sheffield"
+    #         }
+    #     ],
+    #     "prices": [
+    #         {
+    #             "date": datetime.now(), # Time of save
+    #             "new": 50.00,
+    #             "used": 35.00,
+    #             "average": 40.23
+    #         }
+    #     ],
+    #     # "editions": [2, 3]  # If this is present, there MUST be corresponding listings in the db with the same version and book number
+    # })
     # 
     # Bring some variations into the datetime by constructing it yourself eg
     # datetime(year, month, day, hour, minute, second, microsecond)
     # Example: datetime(2021, 1, 1, 12, 0, 0, 0)
+    ...
