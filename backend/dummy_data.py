b85d8d5ff428f3b465f46493a5493e961f8a60ea
Matthew Savin <matt.asavin@gmail.com>
2024-03-09 14:30:24 +0000

    add dummy book

--- backend/dummy_data.py
+++ backend/dummy_data.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import json
 from datetime import datetime
 
-with open("backend/config.json") as f:
+with open("config.json") as f:
     data = json.load(f)
     MONGO_DB = data["mongodb"]
 
@@ -11,50 +11,34 @@ db = client["hackathon"]
 listings = db["listings"]
 
 if __name__ == "__main__":
-
-    # Add a listing like this:
-    # listings.insert_one({
-    #     "bookID": 1,
-    #     "editionID": 1,
-    #     "title": "Example Listing",
-    #     "description": "This is an example listing.",
-    #     "price": 100.00,
-    #     "image": "https://example.com/image.jpg",
-    #     "date": datetime.now(),
-    #     "offers": [
-    #         {
-    #             "price": 100.00,
-    #             "date": datetime.now(),
-    #             "seller": 1,
-    #             "condition": "new",
-    #             "notes": None,
-    #             "location": "Sheffield"
-    #         },
-    #         {
-    #             "price": 90.00,
-    #             "date": datetime.now(),
-    #             "seller": 2,
-    #             "condition": "used",
-    #             "notes": "This is a note.",
-    #             "location": "Newcastle"
-    #         }
-    #     ],
-    #     "prices": [
-    #         {
-    #             "date": datetime.now(),
-    #             "new": 100.00,
-    #             "used": 90.00,
-    #             "average": 95.00
-    #         },
-    #         {
-    #             "date": datetime.now(),
-    #             "new": 100.00,
-    #             "used": 90.00,
-    #             "average": 95.00
-    #         }
-    #     ],
-    #     "editions": [2, 3] If this is present, there MUST be corresponding listings in the db with the same version and book number
-    # })
+    listings.insert_one({
+        "bookID": 9780262367509,
+        "editionID": 4,
+        "title": "Introduction to Algorithms, Fourth Edition",
+        "description": "A comprehensive update of the leading algorithms text, with new material on matchings in bipartite graphs, online algorithms, machine learning, and other topics.",
+        "price": 150.00,
+        "image": "https://mit-press-us.imgix.net/covers/9780262046305.jpg",
+        "date": datetime.now(),
+        "offers": [
+            {
+                "price": 60.00,
+                "date": datetime.now(),
+                "seller": 1,
+                "condition": "new",
+                "notes": None,
+                "location": "Sheffield"
+            }
+        ],
+        "prices": [
+            {
+                "date": datetime.now(),
+                "new": 50.00,
+                "used": 35.00,
+                "average": 40.23
+            }
+        ],
+        # "editions": [2, 3]  # If this is present, there MUST be corresponding listings in the db with the same version and book number
+    })
     # 
     # Bring some variations into the datetime by constructing it yourself eg
     # datetime(year, month, day, hour, minute, second, microsecond)
