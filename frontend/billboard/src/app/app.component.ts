674c7df698857ad866b6c06a7ee65b0bfb9e187e
fn <64966099+f2reninj5@users.noreply.github.com>
2024-03-09 14:20:40 +0000

    Created frontend components

--- frontend/billboard/src/app/app.component.ts
+++ frontend/billboard/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ListingComponent } from "./listing/listing.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, RouterOutlet, ListingComponent]
 })
 export class AppComponent {
   title = 'billboard';
---
+++ frontend/billboard/src/app/listing/sales-history/sales-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-history.component.html',
+  styleUrl: './sales-history.component.scss'
+})
+export class SalesHistoryComponent {
+
+}
